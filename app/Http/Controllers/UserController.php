1e92655175f1a16360119ab8dcc78c5c96ee7399
mj <cody.github.me@gmail.com>
2022-08-28 09:52:00 +0800

    d

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function user_data(){
+    public function user(){
         return response()->json([
             "status" => "OK",
             "message" => "HEroku is awesome!"
